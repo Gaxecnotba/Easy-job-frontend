4a22061331d68cccb24bc2ed5931964049477bbd
Richard <richardardilaz17@hotmail.com>
2024-08-14 17:46:47 -0400

    created form register with all field

--- easyjob/src/components/Register.jsx
+++ easyjob/src/components/Register.jsx
@@ -8,6 +8,13 @@ export function Register() {
   const [user, setUser] = useState({
     email: "",
     password: "",
+    firstName: "",
+    lastName: "",
+    username: "",
+    phone: "",
+    country: "",
+    province: "",
+    city: "",
   });
   const { signup } = useAuth();
   const navigate = useNavigate();
@@ -28,6 +35,13 @@ export function Register() {
           {
             uid: uid,
             email: email,
+            username: user.username,
+            name: user.firstName,
+            lastName: user.lastName,
+            phone: user.phone,
+            countryName: user.country,
+            provinceName: user.province,
+            cityName: user.city,
           }
         );
         if (usercreated.data.message === "User created successfully") {
@@ -43,14 +57,147 @@ export function Register() {
 
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="w-[350px]">
+      <div className="w-[600px]">
         {error && <Alert message={error} />}
 
         <form
           onSubmit={handleSubmit}
           className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
         >
-          <div className="mb-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4">
+            <div>
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="firstName"
+              >
+                First Name
+              </label>
+              <input
+                type="text"
+                name="firstName"
+                placeholder="First Name"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="lastName"
+              >
+                LastName
+              </label>
+              <input
+                type="text"
+                name="lastName"
+                placeholder="Last Name"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+
+          <div className="mb-3">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="phone"
+            >
+              Username
+            </label>
+            <input
+              type="text"
+              name="username"
+              placeholder="nickname or username"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="mb-3">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="phone"
+            >
+              Phone
+            </label>
+            <input
+              type="text"
+              name="phone"
+              placeholder="123-456-7890"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-100 leading-tight focus:outline-none focus:shadow-outline"
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="mb-3">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Country
+            </label>
+            <div>
+              <select
+                name="country"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                onChange={handleChange}
+              >
+                <option
+                  type="text"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                >
+                  Country
+                </option>
+                <option value="01">Canada</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="mb-3">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Province
+            </label>
+            <div>
+              <select
+                name="province"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                onChange={handleChange}
+              >
+                <option value="">Province</option>
+                <option value="01">Alberta</option>
+                <option value="02">British Columbia</option>
+                <option value="03">Manitoba</option>
+                <option value="04">New Brunswick</option>
+                <option value="05">Ontario</option>
+                <option value="06">Quebec</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="mb-3">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              City
+            </label>
+            <div>
+              <select
+                name="city"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                onChange={handleChange}
+              >
+                <option value="">City </option>
+                <option value="0102">Edmonton</option>
+                <option value="0201">Vancouver</option>
+                <option value="0202">Victoria</option>
+                <option value="0301">Winnipeg</option>
+                <option value="0302">Brandon</option>
+                <option value="0401">Fredericton</option>
+                <option value="0402">Moncton</option>
+                <option value="0501">Toronto</option>
+                <option value="0502">Ottawa</option>
+                <option value="0601">Montreal</option>
+                <option value="0602">Quebec City</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="mb-3">
             <label
               className="block text-gray700 text-sm font-fold mb-2"
               htmlFor="email"
@@ -66,7 +213,7 @@ export function Register() {
             />
           </div>
 
-          <div className="mb-4">
+          <div className="mb-3">
             <label
               className="block text-gray700 text-sm font-fold mb-2"
               htmlFor="password"
@@ -87,7 +234,7 @@ export function Register() {
             Register
           </button>
         </form>
-        <p className="my-4 text-sm flex justify-between px-3">
+        <p className="my-3 text-sm flex justify-between px-3">
           Already have an Account?<Link to={"/login"}>Login</Link>
         </p>
       </div>
