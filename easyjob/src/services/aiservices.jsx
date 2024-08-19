483738f8b32398d045349eb68a170c786929395a
Richard <richardardilaz17@hotmail.com>
2024-08-19 19:44:38 -0400

    changed the prompt

--- easyjob/src/services/aiservices.jsx
+++ easyjob/src/services/aiservices.jsx
@@ -9,7 +9,7 @@ const model = genAi.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 export async function getSuggestions(description, style = "neutral") {
   try {
-    const prompt = `Please provide improvements for the following job description in a ${style} tone:\n\nDescription: ${description}, please do not include about Job title or compay name, the user are post easy jobs to do without any experience, the user are looking for a person to do the job in a short time and with a low budget.`;
+    const prompt = `Please provide improvements for the following job description in a ${style} tone:\n\nDescription: ${description}, please do not include about Job title or company name, the user is looking for a person to do the job in a short time. Give me the answer in plain text , at least two paragraphs separate with \n and give me the response more relax.`;
 
     const result = await model.generateContent({
       contents: [
@@ -29,6 +29,7 @@ export async function getSuggestions(description, style = "neutral") {
     });
 
     const text = await result.response.text();
+    console.log(text);
 
     if (!text) {
       throw new Error("No content found in the response");
